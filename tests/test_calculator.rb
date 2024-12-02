c45aa1a17a2c1d8e522d6ccc32523096ff5ca987
Zackery Bangs <106060493+zbangs@users.noreply.github.com>
2024-12-02 20:15:15 +0000

    Separate test methods

--- tests/test_calculator.rb
+++ tests/test_calculator.rb
@@ -7,6 +7,10 @@ class TestCalculator < Minitest::Test
   def test_addition
     calculator = Calculator.new
     assert_equal 4, calculator.add(2,2), "Addition method failed"
+  end
+
+  def test_subtraction
+    calculator = Calculator.new
     assert_equal 0, calculator.subtract(2,2,), "Subtraction method failed"
   end
 end
